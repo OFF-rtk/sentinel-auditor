44d74e6d4838fe532e8830c14d7c1d878937f571
OFF-rtk <mail.offrtk@gmail.com>
2025-12-24 13:02:09 +0530

    Dockerized Redis and setup redis commander for visualization of the cache stream

--- auditor/api/agents/enforcer.py
+++ auditor/api/agents/enforcer.py
@@ -1,7 +1,14 @@
 import redis
 import os
 
-r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0, decode_responses=True)
 
 def is_user_blacklisted(user_id: str):
     """
